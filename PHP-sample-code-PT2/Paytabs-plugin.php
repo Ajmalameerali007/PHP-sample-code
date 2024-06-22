68da0df4720d4c5e3929bcd368753ac7dab0fe5e
Ajmalameerali007 <ajmalameerali@gmail.com>
2024-06-22 19:01:33 +0400

    Update Create-payment-with-token.php

--- PHP-sample-code-PT2/Paytabs-plugin.php
+++ PHP-sample-code-PT2/Paytabs-plugin.php
@@ -3,33 +3,12 @@
 
 class Paytabs
 {
-    const   PROFILE_ID = 59305,
-        SERVER_KEY = 'S9JN9NTLRW-JBD9GGBBGN-DRLNJBBGTG',
-        BASE_URL = 'https://secure-egypt.paytabs.com/';
+    const   PROFILE_ID = 142466,
+        SERVER_KEY = 'SHJ9RGHJZN-JJHMRRG6BM-NJ6KJJK2ND',
+        BASE_URL = 'https://secure.paytabs.com/';
 
 
-    function send_api_request($request_url, $data, $request_method = null)
-    {
-        $data['profile_id'] = $this::PROFILE_ID;
-        $curl = curl_init();
-        curl_setopt_array($curl, array(
-            CURLOPT_URL => $this::BASE_URL . $request_url,
-            CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_ENCODING => '',
-            CURLOPT_MAXREDIRS => 10,
-            CURLOPT_TIMEOUT => 0,
-            CURLOPT_CUSTOMREQUEST => isset($request_method) ? $request_method : 'POST',
-            CURLOPT_POSTFIELDS => json_encode($data, true),
-            CURLOPT_HTTPHEADER => array(
-                'authorization:' . $this::SERVER_KEY,
-                'Content-Type:application/json'
-            ),
-        ));
-
-        $response = json_decode(curl_exec($curl), true);
-        curl_close($curl);
-        return $response;
-    }
+    
 
     function getBaseUrl()
     {
--- PHP-sample-code-PT2/Token-based-transaction/Create-payment-with-token.php
+++ PHP-sample-code-PT2/Token-based-transaction/Create-payment-with-token.php
@@ -4,11 +4,11 @@ $plugin = new Paytabs();
 
 $request_url = 'payment/request';
 $data = [
-    "tran_type" => "capture",
+    "tran_type" => "auth",
     "tran_class" => "recurring",
     "cart_id" => "cart_444441",
-    "cart_currency" => "EGP",
-    "cart_amount" => 120,
+    "cart_currency" => "aed",
+    "cart_amount" => 10,
     "cart_description" => "Description of the items/services",
     "token" => "2C4651BE67A3EA30C6B791F8618B78B0",  //token you received in the create page with token request
     "tran_ref" => "TST2105600089454", // trans_ref you received in the create page with token request
